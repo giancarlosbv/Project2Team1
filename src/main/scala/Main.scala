1b3ad75f7e398b1aacc29e2e548e18a669edd7bd
Giancarlos Blanco <giancarlosbv@gmail.com>
2022-02-14 10:45:43 -0500

    query methods plugged into main menu

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -37,25 +37,25 @@ object Main {
             scanner.nextLine()
             menuChoice match {
                 case 1 =>
-                    println("case 1")
+                    MedianAgeOfVaccinatedPeopleBasedOffLocation()
                 case 2 =>
-                    println("case 2")
+                    DeathsAmongVaccinatedPeopleBetweenAges65And70()
                 case 3 =>
-                    println("case 3")
+                    MedianAgeOfDeath()
                 case 4 =>
-                    println("case 4")
+                    NewCasesInPeople70Plus()
                 case 5 =>
-                    println("case 5")
+                    DeathsVSVaccinationPerContinent()
                 case 6 =>
-                    println("case 6")
+                    LifeExpectancyOfPeople70Plus()
                 case 7 =>
-                    println("case 7")
+                    ContinentWithMostCasesInA30DayPeriod
                 case 8 =>
-                    println("case 8")
+                    NewAndTotalCases_NewAndTotalDeaths_InPeople65Plus_PerContinent
                 case 9 =>
-                    println("case 9")
+                    PopulationDensityVsTotalVaccination_Cases_Deaths
                 case 10 =>
-                    println("case 10")
+                    TotalCovidCasesInLocationsWhereTotalVaccinationRateIsAbove50Percent
                 case 11 =>
                     endProgram = true
                 case _ =>
@@ -64,21 +64,16 @@ object Main {
             }
 
         }
-        }
-
-        
-
         //Method to calculate the median age of vaccinated people related to their location
         //Fields: Median_Age, Total_Vaccinations, Location
-        def MedianAgeOfVaccinatedPeopleBasedOffLocation():UNIT 
-        {
-
+        def MedianAgeOfVaccinatedPeopleBasedOffLocation(): Unit =
+        {   println("method 1")
 
         }
 
         //Method to calculate the number of deaths among vaccinated people between the age of 65 and 70
         //Fields: New_Deaths, People_Vaccinated, Total_Vaccinations
-        def DeathsAmongVaccinatedPeopleBetweenAges65And70():UNIT 
+        def DeathsAmongVaccinatedPeopleBetweenAges65And70():Unit =  
         {
 
 
@@ -87,7 +82,7 @@ object Main {
 
         //Method to calculate the median age of death
         //Fields: Median_Age, Total_Deaths
-        def MedianAgeOfDeath():UNIT 
+        def MedianAgeOfDeath():Unit =  
         {
 
 
@@ -95,7 +90,7 @@ object Main {
 
         //Method to calculate the number of new cases in people 70 and older and also their mortality rate
         //Fields: New_Cases, Aged_70_Older, New_Deaths, Total_Deaths
-        def NewCasesInPeople70Plus():UNIT 
+        def NewCasesInPeople70Plus():Unit =  
         {
 
 
@@ -103,7 +98,7 @@ object Main {
 
         //Method to calculate percentage of deaths and vaccinations in a contintent and comapare
         //Fields: Continent, Total_Deaths, People_Vaccinated
-        def DeathsVSVaccinationPerContinent():UNIT 
+        def DeathsVSVaccinationPerContinent():Unit =  
         {
 
 
@@ -111,7 +106,7 @@ object Main {
 
         //Method to calculate the life expectancy of people 70 and up who contracted the virus
         //Fields: Life_Expectancy, Aged_70_Older
-        def LifeExpectancyOfPeople70Plus():UNIT 
+        def LifeExpectancyOfPeople70Plus():Unit =  
         {
 
 
@@ -120,7 +115,7 @@ object Main {
 
         //Method to calculate which continent had the largest number of cases in a 30 day period
         //Fields: Location, Total_Cases, New_Cases, Date
-        def ContinentWithMostCasesInA30DayPeriod():UNIT 
+        def ContinentWithMostCasesInA30DayPeriod():Unit =  
         {
 
 
@@ -128,7 +123,7 @@ object Main {
 
         //Method to calculate all this stuff
         //Fields: New_Cases, Total_Cases, New Deaths, Total_Deaths, Aged_65_Older, Continent, Location
-        def NewAndTotalCases_NewAndTotalDeaths_InPeople65Plus_PerContinent():UNIT 
+        def NewAndTotalCases_NewAndTotalDeaths_InPeople65Plus_PerContinent():Unit =  
         {
 
 
@@ -137,7 +132,7 @@ object Main {
         //Method to calculate Population Density and compare it to Total Vaccinations, Cases, and Deaths
         //Fields: Population_Density, Population, Continent, Location, Date, Total_Cases, New_Cases, Total_Deaths
             //New_Deaths, New_Tests, Total_Tests, Total_Vaccinations
-        def PopulationDensityVsTotalVaccination_Cases_Deaths():UNIT 
+        def PopulationDensityVsTotalVaccination_Cases_Deaths():Unit =  
         {
 
 
@@ -145,11 +140,16 @@ object Main {
 
         //Method to calculate total Covid cases in places where more then half of the population is vaccinated
         //Fields: Population, Location, New_Cases, People_Fully_Vaccinated
-        def TotalCovidCasesInLocationsWhereTotalVaccinationRateIsAbove50Percent():UNIT 
+        def TotalCovidCasesInLocationsWhereTotalVaccinationRateIsAbove50Percent():Unit =  
         {
 
 
         }
+        }
+
+        
+
+
 
 
 
