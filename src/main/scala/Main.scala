765f8f88ecd6e4d44d6a274f5169fb4639f8012b
Giancarlos Blanco <giancarlosbv@gmail.com>
2022-02-14 10:33:07 -0500

    menu options, match case added

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.sql.SparkSession
-
+import java.util.Scanner
+import scala.util.matching.Regex.Match
 object Main {
     def main(args:Array[String]): Unit = {
         val spark = 
@@ -10,6 +11,9 @@ object Main {
             .config("spark.eventLog.enabled", "false")
             .getOrCreate()
             
+        
+
+
         sparkCovidData()
 
 
@@ -17,18 +21,52 @@ object Main {
 
 
         def sparkCovidData():Unit = {
+            var scanner = new Scanner(System.in)
+
             println("====================")
             println("Welcome to Team 1's Spark Covid Data Analysis")
-            println("====================")
             var endProgram = false
             while (!endProgram){
-                mainmenu()
+                mainMenu()
             }
+
+            def mainMenu():Unit = {
+            println("====================")
+            println("Main menu")
+            var menuChoice = scanner.nextInt()
+            scanner.nextLine()
+            menuChoice match {
+                case 1 =>
+                    println("case 1")
+                case 2 =>
+                    println("case 2")
+                case 3 =>
+                    println("case 3")
+                case 4 =>
+                    println("case 4")
+                case 5 =>
+                    println("case 5")
+                case 6 =>
+                    println("case 6")
+                case 7 =>
+                    println("case 7")
+                case 8 =>
+                    println("case 8")
+                case 9 =>
+                    println("case 9")
+                case 10 =>
+                    println("case 10")
+                case 11 =>
+                    endProgram = true
+                case _ =>
+                    println("Invalid Input, Try again")
+                    mainMenu()
+            }
+
+        }
         }
 
-        def mainmenu():Unit = {
-            println
-        }
+        
 
 
 
